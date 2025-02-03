a11616bc32225412070606d4f35886b4f8ae0b44
Shivam <shivamgupta90353@gmail.com>
2025-02-03 12:12:39 +0530

    first commit

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,7 +1,17 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+// import { defineConfig } from 'vite'
+// import react from '@vitejs/plugin-react-swc'
 
-// https://vite.dev/config/
+// // https://vite.dev/config/
+// export default defineConfig({
+//   plugins: [react()],
+// })
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react-swc";
+
 export default defineConfig({
   plugins: [react()],
-})
+  server: {
+    port: parseInt(process.env.PORT) || 3000, // Use Render's assigned port
+    host: "0.0.0.0", // Allow external access
+  },
+});
