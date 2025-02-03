601a9a38a820fa18a8ee9e90eca11a0918cf54a8
Shivam <shivamgupta90353@gmail.com>
2025-02-03 12:35:49 +0530

    first ci

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -11,8 +11,7 @@ import react from "@vitejs/plugin-react-swc";
 export default defineConfig({
   plugins: [react()],
   server: {
-    port: parseInt(process.env.PORT) || 3000, // Use Render's assigned port
+    port: parseInt(process.env.PORT) || 8000, // Use Render's assigned port
     host: "0.0.0.0", // Allow external access
-    allowedHosts: ["crud-flask-xwmq.onrender.com"], // Add your host here
   },
 });
